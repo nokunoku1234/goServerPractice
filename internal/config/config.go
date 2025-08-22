9dc212a645e2cc9badeee1142b7220b2a5ed5ff5
奥野領太 <>
2025-08-23 02:49:51 +0900

    update config

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,13 +2,17 @@ package config
 
 import (
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 )
 
 type Config struct {
-	Port  string
-	DBUrl string
+	Port                string
+	DBUrl               string
+	JWTSecret           string
+	JWTExpires          time.Duration
+	RefreshTokenExpires time.Duration
 }
 
 func Load() Config {
@@ -21,5 +25,23 @@ func Load() Config {
 	if dbUrl == "" {
 		panic("DB NOT FOUND")
 	}
-	return Config{Port: port, DBUrl: dbUrl}
+	jwtSecret := os.Getenv("JWT_SECRET")
+	if len(jwtSecret) < 32 {
+		panic("JWT_SECRET must be at least 32 characters long")
+	}
+	jwtExpires, err := time.ParseDuration(os.Getenv("JWT_EXPIRES"))
+	if err != nil {
+		jwtExpires = 24 * time.Hour
+	}
+	refreshTokenExpires, err := time.ParseDuration(os.Getenv("REFRESH_TOKEN_EXPIRES"))
+	if err != nil {
+		refreshTokenExpires = 168 * time.Hour
+	}
+	return Config{
+		Port:                port,
+		DBUrl:               dbUrl,
+		JWTSecret:           jwtSecret,
+		JWTExpires:          jwtExpires,
+		RefreshTokenExpires: refreshTokenExpires,
+	}
 }
