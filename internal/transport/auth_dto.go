9f639b544450d8f3857b94c11bf621f0e20f88ec
奥野領太 <>
2025-08-23 12:27:04 +0900

    update AuthDTO

--- internal/transport/auth_dto.go
+++ internal/transport/auth_dto.go
@@ -13,10 +13,22 @@ type CreateUserResponse struct {
 	RefreshToken string  `json:"refreshToken"`
 }
 
+type LoginRequest struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
+}
+
+type LoginResponse struct {
+	User         UserDTO `json:"user"`
+	AccessToken  string  `json:"accessToken"`
+	RefreshToken string  `json:"refreshToken"`
+}
+
 type UserDTO struct {
 	ID        int       `json:"id"`
 	Email     string    `json:"email"`
 	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt,omitempty"`
 }
 
 type ErrorResponse struct {
