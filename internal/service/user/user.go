ad982933543b966fd92b8d3ec9998c83464c2197
奥野領太 <>
2025-08-23 12:26:50 +0900

    メールアドレスをもとにDBのユーザーを引っ張ってくる処理追加

--- internal/service/user/user.go
+++ internal/service/user/user.go
@@ -10,3 +10,11 @@ func ExistsByEmail(client *ent.Client, ctx context.Context, email string) (bool,
 	exists, err := client.User.Query().Where(user.EmailEQ(email)).Exist(ctx)
 	return exists, err
 }
+
+func FindByEmail(client *ent.Client, ctx context.Context, email string) (*ent.User, error) {
+	u, err := client.User.
+		Query().
+		Where(user.EmailEQ(email)).
+		Only(ctx)
+	return u, err
+}
